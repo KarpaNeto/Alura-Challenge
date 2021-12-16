29ecf96a92cfd7f8e4ce9c2cba7199a581a4ef13
Pedro Krapinski Neto <pedrokn@gmail.com>
2021-12-15 22:27:06 -0300

    commit

--- js/change-color.js
+++ js/change-color.js
@@ -1,5 +1,12 @@
-function color(value) {
-    var corborda = document.getElementById('border').style.backgroundColor=value;
-    console.log(corborda)
+function colorSelect(value) {
+    var selectColor = document.getElementById('select');
+   selectColor.style.backgroundColor = value;
 }
+
+function colorBox(value) {
+    var borderColor = document.getElementById('border');
+   borderColor.style.backgroundColor = value;
+}
+
+
 
