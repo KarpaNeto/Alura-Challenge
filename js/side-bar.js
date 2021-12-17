aa74c414d625379aba126185621f0afc3902d7bc
Pedro Krapinski Neto <pedrokn@gmail.com>
2021-12-17 00:12:45 -0300

    commit

---
+++ js/side-bar.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $(".mobile-iconbar").click(function () {
+        $(this).toggleClass("active");
+        $(".menu-side").toggleClass("active");
+        })
+    })
